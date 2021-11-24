9ccb3b5f50ced6f9c7bd4a036052d7142d24f15f
reenzzop <renzops2017@gmail.com>
2021-11-24 18:50:34 -0500

    Proyecto Integrador - OCR

---
+++ src/app/home-file/home-file.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {OcrService} from '../ocr.service';
+
+@Component({
+  selector: 'app-home-file',
+  templateUrl: './home-file.component.html',
+  styleUrls: ['./home-file.component.scss']
+})
+export class HomeFileComponent implements OnInit {
+  listImages = [
+    {
+      src: 'Cita1.JPG'
+    },
+    {
+      src: 'Cita2.jpg'
+    },
+    {
+      src: 'Cita3.jpg'
+    },
+    {
+      src: 'Cita4.jpg'
+    }
+  ];
+
+  constructor(private ocrService: OcrService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  clickImage = (image) => {
+    this.ocrService.cbImage.emit(image);
+  };
+}
